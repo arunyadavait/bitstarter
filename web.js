e708c415c0d56dc4075e6c1b1402575b2245f628
Arun Yadav <arunyadavait@gmail.com>
2013-07-10 05:59:15 +0000

    changes made to readFileSync

--- web.js
+++ web.js
@@ -4,13 +4,13 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-  response.send(buf.toString("utf-8",0,buf.length));
+  response.send(buf.toString());
 });
 
-var buf = fs.readFile('index.html',function (err,data){
-  if(err) throw err;
-   console.log(data);
-});
+var fs = require('fs');
+var buf = fs.readFileSync('index.html');
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
