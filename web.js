d6f85f34852f5109d21fc36e2dc748891e4884b4
Arun Yadav <arunyadavait@gmail.com>
2013-07-10 04:52:20 +0000

    correct readFile

--- web.js
+++ web.js
@@ -6,12 +6,11 @@ app.get('/', function(request, response) {
 
   response.send(buf.toString("utf-8",0,buf.length));
 });
-
-var buf = new Buffer(30);
-buf = fs.readfile('index.html',function (err,data){
+    var buf = new Buffer(30);
+buf = fs.readFile('index.html',function (err,data){
   if(err) throw err;
    console.log(data);
-}); 
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
