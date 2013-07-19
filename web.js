7903e4cb99f539253373ea75ca19933eae3374a1
Arun Yadav <arunyadavait@gmail.com>
2013-07-19 11:42:16 +0000

    change in var app in web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express();
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
@@ -12,7 +12,7 @@ var buf = fs.readFileSync('index.html');
 
 
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
